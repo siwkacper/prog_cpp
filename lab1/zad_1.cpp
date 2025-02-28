30b1c37165030b22f510f28f50337884c39f2649
kacsiw <siwkacper@gmail.com>
2025-02-28 11:13:39 +0100

    Implemented a compliment function and added better interface.

--- lab1/zad_1.cpp
+++ lab1/zad_1.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
 using namespace std;
 
+void compliment(char in_nuc){
+
+    cout << "Complimentary nucleotide is ";
+
+    switch(in_nuc){
+        case 'A':
+            cout << 'T'<< endl;
+            break;
+        case 'T':
+            cout << 'A'<< endl;
+            break;
+        case 'C':
+            cout << 'G'<< endl;
+            break;
+        case 'G':
+            cout << 'C'<< endl;
+            break;
+        default:
+            cout << "Input is not a nucleotide symbol\n";
+    }
+
+}
+
 int main(){
-    char in_nuc='N',compliment='N'; /*Initialize nucleotides as any*/
+    char in_nuc='N'; /*Initialize nucleotide as any*/
+    cout << "Input a nucleotide symbol [A,T,C,G]: ";
     cin >> in_nuc;
-
-    
-
+    compliment(in_nuc);
     return 0;
 }
